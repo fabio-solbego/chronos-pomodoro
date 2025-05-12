c70e2c87a256f2287c13bec7c2967ef05f975f69
Fábio Solbego <fabiosolbego1005@gmail.com>
2025-05-12 15:55:40 -0300

    Criação Headings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Heading } from './components/Heading';
+
+
 import './styles/theme.css';
 import './styles/global.css';
+
+
+
 export function App() {
   console.log('Oi');
 
   return (
     <>
-    <h1>Olá Mundo!</h1>
+    <Heading/>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque,
         consectetur nulla. Voluptatem cumque ducimus saepe obcaecati corrupti
