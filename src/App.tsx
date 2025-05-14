6720dfaf04d8c48070141393c89173fc78c788a0
Fábio Solbego <fabiosolbego1005@gmail.com>
2025-05-14 15:49:35 -0300

    Containers modificação

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
-import { Heading } from './components/Heading';
-
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
 
 
 
 export function App() {
  return (
-    <>
-    <Heading>Olá mundo! 1
+ <>
+   <Container>
+    <Heading>LOGO</Heading>
 
-      <button>
-        <TimerIcon/>
-        </button>
+   </Container>
 
-    </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque,
-        consectetur nulla. Voluptatem cumque ducimus saepe obcaecati corrupti
-        provident natus l abore consequatur nam? Autem, exercitationem. Dolorem
-        at cumque consequuntur, enim odio aspernatur iure amet. Magnam,
-        expedita? Commodi sed minus atq ue reiciendis, cumque esse. Voluptas
-        magni facere nostrum vitae perferendis, fugit quisquam?
-      </p>
+<Container>
+<Heading>MENU</Heading>
+    </Container> 
       </>
-    
-  );
+ );
 }
