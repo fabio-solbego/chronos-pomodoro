cf069cac6aeef1d64f3e6b88d3f049844effbc5f
Fábio Solbego <fabiosolbego1005@gmail.com>
2025-05-20 12:47:03 -0300

    Pages e Templates

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,31 @@
+import { PlayCircleIcon } from "lucide-react";
+import { Cycles } from "../Cycles";
+import { DefaltButton } from "../DefaultButton";
+import { DefaltInput } from "../DefaultInput";
+
+export function MainForm() {
+  return (
+    <form className='form' action=''>
+      <div className='formRow'>
+        <DefaltInput
+          labelText='task'
+          id='meuInput'
+          type='text'
+          placeholder='Digite algo'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Lorem ipsum dolor sit amet</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaltButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
