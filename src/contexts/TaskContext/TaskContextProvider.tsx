396653ced11a9ceeec228271ade15d2a29bd6ec1
Fábio Solbego <fabiosolbego1005@gmail.com>
2025-06-17 10:23:20 -0300

    tste

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,25 +1,20 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
-
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, setState] = useState(initialTaskState);
 
-  const [numero, dispatch] = useReducer((state, action) => {
-    return state;
-  }, 0);
-
-  //  useEffect(() => {
-  // console.log(state);
-  // }, [state]);
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <TaskContext.Provider value={{ state, setState }}>
-      <h1> O número é: {numero}</h1>
+      {children}
     </TaskContext.Provider>
   );
 }
